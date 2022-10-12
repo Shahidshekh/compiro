d02481f5e5f0dc586d89065abf1255b6c261f75c
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-10-12 08:54:04 -0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,22 @@
 
+import logging
+import logging.handlers
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        logging.FileHandler(
+            filename="./log.txt",
+            mode='w'
+        ),
+        logging.StreamHandler(sys.stdout)
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
