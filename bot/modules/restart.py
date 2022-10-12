907890ad9d33b207b6992ae1f2aa61dcf9119ae4
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-10-12 09:24:59 -0700

    Update restart.py

--- bot/modules/restart.py
+++ bot/modules/restart.py
@@ -5,4 +5,7 @@ from os import execl
 def restart(app, message):
   msg = await message.reply("**Restarting.....**", quote=True)
   srun(["python3", "upstream.py"])
+  with open(".restartmg", "w") as f:
+    f.truncate(0)
+    f.write(f"{msg.chat.id}\n{msg.message_id}\n")
   execl(executable, executable, "-m", "bot")
