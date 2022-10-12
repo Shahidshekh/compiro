de497404c2d8e4bb7d02043351916b38f5dc2d5e
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-10-12 09:18:15 -0700

    Update restart.py

--- bot/modules/restart.py
+++ bot/modules/restart.py
@@ -1,8 +1,8 @@
 from subprocess import run as srun
 from sys import executable
+from os import execl
 
 def restart(app, message):
   msg = await message.reply("**Restarting.....**", quote=True)
-  clean_all()
   srun(["python3", "upstream.py"])
-  
+  execl(executable, executable, "-m", "bot")
